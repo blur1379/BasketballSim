ed4dbcb9de43d620f6ce72aaee3dffadd11a15bf
mohammad blur <blor1379@gmail.com>
2024-08-16 19:04:35 +0330

    :lipstick: adaptive activity background for standby

--- GameWidget/LiveActivityView.swift
+++ GameWidget/LiveActivityView.swift
@@ -9,52 +9,50 @@ import SwiftUI
 import WidgetKit
 
 struct LiveActivityView: View {
+    @Environment(\.isActivityFullscreen) var isStandby
+    let context: ActivityViewContext<GameAttributes>
     var body: some View {
         ZStack {
-            Image("activity-background")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .overlay {
-                    ContainerRelativeShape()
-                        .fill(.black.opacity(0.3).gradient)
-                }
+            if !isStandby {
+                Image("activity-background")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .overlay {
+                        ContainerRelativeShape()
+                            .fill(.black.opacity(0.3).gradient)
+                    }
+            }
+            
             VStack(spacing: 12) {
                 HStack {
-                    Image("warriors")
+                    Image(context.attributes.homeTeam)
                         .teamLogoModifier(frame: 60)
                     
-                    Text("105")
+                    Text("\(context.state.gameState.homeScore)")
                         .font(.system(size: 40,weight: .bold))
                         .foregroundStyle(.white.opacity(0.9))
                     Spacer()
                     
-                    Text("105")
+                    Text("\(context.state.gameState.awayScore)")
                         .font(.system(size: 40,weight: .bold))
-                        .foregroundStyle(.black.opacity(0.7))
-                    Image("bulls")
+                        .foregroundStyle(isStandby ? .white : .black.opacity(0.7))
+                    Image(context.attributes.awayTeam)
                         .teamLogoModifier(frame: 60)
                 }
                 
                 HStack {
-                    Image("warriors")
+                    Image(context.state.gameState.scoringTeamName)
                         .teamLogoModifier(frame: 20)
-                    Text("S. curry drains a 3")
+                    Text(context.state.gameState.lastAction)
                         .font(.callout)
                         .fontWeight(.semibold)
                         .foregroundStyle(.white.opacity(0.9))
                 }
             }
         }
+        .padding()
         .containerBackground(for: .widget) {
           
         }
     }
-}
-
-struct LiveActivityView_Previews: PreviewProvider {
-    static var previews: some View {
-        LiveActivityView()
-            .previewContext(WidgetPreviewContext(family: .systemMedium))
-           
-    }
-}
+} 
